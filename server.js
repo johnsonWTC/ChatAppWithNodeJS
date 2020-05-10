9956f8e093942cd49dd54b5a4791855e25ecb074
Johnson Dubula <jdubula@iqbusiness.net>
2020-05-10 20:06:06 +0200

    setting up the chat

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ io.sockets.on('connection', function(socket) {
     socket.on('disconnect', function(data) {
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconneted %s sockets connected', connections.length);
-
-
     })
 
     //send message 
 
-
+    socket.on('send message', function(data) {
+        console.log(data);
+        io.sockets.emit('new message', { msg: data });
+    })
 
 
 
